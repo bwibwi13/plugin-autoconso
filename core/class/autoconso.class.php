1c5d1ff253acc0f94ea94c976d491c4d91ab0a05
Bwibwi13 <bwibwi13@gmail.com>
2022-07-17 20:55:30 +0200

    Update and rename template.class.php to autoconso.class.php

--- core/class/autoconso.class.php
+++ core/class/autoconso.class.php
@@ -18,7 +18,7 @@
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
-class template extends eqLogic {
+class autoconso extends eqLogic {
   /*     * *************************Attributs****************************** */
 
   /*
@@ -141,7 +141,7 @@ class template extends eqLogic {
 
 }
 
-class templateCmd extends cmd {
+class autoconsoCmd extends cmd {
   /*     * *************************Attributs****************************** */
 
   /*
