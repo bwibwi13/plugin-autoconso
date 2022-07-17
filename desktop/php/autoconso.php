8d8690e8c1be3e2124b847e60a34ea90a73a3180
Bwibwi13 <bwibwi13@gmail.com>
2022-07-17 20:58:43 +0200

    Update autoconso.php

--- desktop/php/autoconso.php
+++ desktop/php/autoconso.php
@@ -203,6 +203,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 </div><!-- /.row row-overflow -->
 
 <!-- Inclusion du fichier javascript du plugin (dossier, nom_du_fichier, extension_du_fichier, id_du_plugin) -->
-<?php include_file('desktop', 'template', 'js', 'template');?>
+<?php include_file('desktop', 'autoconso', 'js', 'autoconso');?>
 <!-- Inclusion du fichier javascript du core - NE PAS MODIFIER NI SUPPRIMER -->
 <?php include_file('core', 'plugin.template', 'js');?>
